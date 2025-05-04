4c87fb8cddbef8f623bd9401b864e0f67990a7ec
Safmica <safmikasa@gmail.com>
2025-05-04 11:28:53 +0700

    fix: routing when destory and button for delete content

--- resources/views/file.blade.php
+++ resources/views/file.blade.php
@@ -45,13 +45,15 @@
                 <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-32 bg-white rounded-lg shadow-md">
                     <ul>
                         <li><a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Edit</a></li>
-                        <li>
-                            <form action="{{ route('courses.destroy', $course->id) }}" class="block px-4 py-2 text-gray-700 hover:bg-gray-100" method="POST" onsubmit="return confirm('Are you sure?')">
+                        <li class="hover:bg-gray-100">
+                            <form action="{{route('courses.meetings.files.destroy', [$course->id,$meeting->id, $file->id])}}" method="POST" class="w-full">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="text-red-500">Delete</button>
+                                <button type="submit" class="w-full text-left px-4 py-2 text-red-500">
+                                    Delete
+                                </button>
                             </form>
-                        </li>
+                        </li>                    
                     </ul>
                 </div>
             </div>    
