5f81306a3b0e62055dfa6c45df32e227c1951d17
Safmica <safmikasa@gmail.com>
2025-05-04 11:19:23 +0700

    style: add placeholder on signup and login

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -16,19 +16,19 @@
             @csrf
             <div>
                 <label class="block text-gray-600 mb-2">Name</label>
-                <input type="text" name="name" required
+                <input type="text" name="name" required placeholder="your name"
                     class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#fcbf49]">
             </div>
 
             <div>
                 <label class="block text-gray-600 mb-2">Email</label>
-                <input type="email" name="email" required
+                <input type="email" name="email" required placeholder="youremail@gmail.com"
                     class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#fcbf49]">
             </div>
 
             <div>
                 <label class="block text-gray-600 mb-2">Password</label>
-                <input type="password" name="password" required
+                <input type="password" name="password" required placeholder="min. 6 char"
                     class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#fcbf49]">
             </div>
 
